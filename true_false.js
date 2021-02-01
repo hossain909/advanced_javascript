b160e8c642e66d488ddde94224ad23d194de841d
hossain909 <70032159+hossain909@users.noreply.github.com>
2021-02-02 02:37:08 +0600

    true false

---
+++ true_false.js
@@ -0,0 +1,22 @@
+// ==================  Falsy Value ================== //
+// false
+// 0
+// ""
+//  null
+// undefined
+// NaN
+// ==================  Truthy Value ================== //
+// true
+// "0"
+// " "
+// {}
+// []
+
+
+const age = 4;
+if(age > 0){
+  console.log("This is True");
+}else{
+  console.log("This is False");
+}
+
