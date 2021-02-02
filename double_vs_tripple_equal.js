934327f422758a4e7a5086388cf560c3aa58dca4
hossain909 <70032159+hossain909@users.noreply.github.com>
2021-02-02 09:58:28 +0600

    double equal and tripple equal

---
+++ double_vs_tripple_equal.js
@@ -0,0 +1,16 @@
+// === Tripple equal checks value and also data type of input
+let num = 0;
+if(num === false){
+  console.log("This condition is true");
+}else{
+  console.log("This condition is false");
+}
+
+
+// Double equal == checks only value then nearest type to convert then compare two values;
+num = 0;
+if(num == false){
+  console.log("This condition is true");
+}else{
+  console.log("This condition is false");
+}
