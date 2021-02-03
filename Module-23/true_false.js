087ab743489e833c81ec75a8f48e375a318d7087
hossain909 <70032159+hossain909@users.noreply.github.com>
2021-02-03 08:22:42 +0600

    Changed folder name

--- Module-23/true_false.js
+++ Module-23/true_false.js
@@ -13,10 +13,15 @@
 // []
 
 
-const age = 4;
-if(age > 0){
-  console.log("This is True");
-}else{
-  console.log("This is False");
-}
+// const age = 4;
+// if(age > 0){
+//   console.log("This is True");
+// }else{
+//   console.log("This is False");
+// }
 
+if("false"){
+  console.log("I am not false");
+}else{
+  console.log("I am false")
+}
